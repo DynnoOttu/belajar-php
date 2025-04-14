b777f9f57538c5ae6607b308f945cfc82f4842dd
Dynno Yohanis Ottu <dynnoottu968@gmail.com>
2025-04-14 21:38:22 +0700

    tipe data number

---
+++ php-dasar/TipeDataNumber.php
@@ -0,0 +1,21 @@
+<?php
+echo "Decimal : ";
+var_dump(12345);
+
+echo "Octal : ";
+var_dump(0123);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111111);
+
+echo "Underscore di number : ";
+var_dump(1_000_000);
+
+echo "Floating Point : ";
+var_dump(10.5);
+
+echo "Integer Overflow : ";
+var_dump(9223372036854775808);
